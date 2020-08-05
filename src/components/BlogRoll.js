ad4fc705c0ab5d4d81a45ea28186fca015ab0f09
ilbrigz <ilogirb23@yahoo.com>
2020-08-05 20:09:43 +0800

    .:

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Link, graphql, StaticQuery } from 'gatsby';
-import PreviewCompatibleImage from './PreviewCompatibleImage';
 
 class BlogRoll extends React.Component {
   render() {
     return <h1>Yahoo from blogRoll</h1>;
   }
 }
+
+export default BlogRoll;
